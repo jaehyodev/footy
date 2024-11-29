a7933ed6af6de0ebdf7052bf604b487c7ac8fedc
Jaehyo <kimjaehyo0623@gmail.com>
2024-11-29 15:21:52 +0900

    feat: 리그 선택 기능 추가

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,14 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import './data/constants.dart';
+import './providers/league_provider.dart';
+import './screens/home/home_screen.dart';
 import './widgets/custom_app_bar.dart';
 import './widgets/bottom_nav_bar.dart';
 import './themes/style.dart';
 
 void main() async {
   runApp(
-    MaterialApp(
-      theme: theme,
-      home: const MyApp(),
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => LeagueProvider()),
+      ],
+      child: MaterialApp(
+        home: const MyApp(),
+        theme: theme,
+      ),
     ),
   );
 }
@@ -22,8 +30,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Scaffold(
       appBar: CustomAppBar(), // 공통 앱바
-      body: Text(
-        "body",
+      body: Padding(
+        padding: bodyPadding, // bodyPadding을 적용
+        child: HomeScreen(), // 현재 화면이 HomeScreen으로 설정
       ),
       bottomNavigationBar: BottomNavBar(), // 공통 바텀 네비게이션 바
     );
