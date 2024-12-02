34ddcb445fdf66bac667f158572a716eec02f5dc
Jaehyo <kimjaehyo0623@gmail.com>
2024-12-02 16:09:00 +0900

    chore: 네비게이션 테스트를 위한 화면 추가

---
+++ frontend/lib/screens/following/following_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FollowingScreen extends StatefulWidget {
+  const FollowingScreen({super.key});
+
+  @override
+  FollowingScreenState createState() => FollowingScreenState();
+}
+
+class FollowingScreenState extends State<FollowingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text('Following Screen'));
+  }
+}
---
+++ frontend/lib/screens/league/league_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LeagueScreen extends StatefulWidget {
+  const LeagueScreen({super.key});
+
+  @override
+  State<LeagueScreen> createState() => LeagueScreenState();
+}
+
+class LeagueScreenState extends State<LeagueScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text('League Screen'));
+  }
+}
