f3c524560dde70de45bfb5889d52fb4ac26d26ba
Jaehyo <kimjaehyo0623@gmail.com>
2024-12-02 17:24:02 +0900

    feat: 바텀네비게이션 기능 구현

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:provider/provider.dart';
-import './data/constants.dart';
-import './providers/league_provider.dart';
-import './providers/date_provider.dart';
-import './screens/home/home_screen.dart';
-import './widgets/custom_app_bar.dart';
-import './widgets/bottom_nav_bar.dart';
-import './themes/style.dart';
+
+import 'package:frontend/data/constants.dart';
+import 'package:frontend/providers/date_provider.dart';
+import 'package:frontend/providers/league_provider.dart';
+import 'package:frontend/screens/following/following_screen.dart';
+import 'package:frontend/screens/home/home_screen.dart';
+import 'package:frontend/screens/league/league_screen.dart';
+import 'package:frontend/screens/news/news_screen.dart';
+import 'package:frontend/screens/settings/settings_screen.dart';
+import 'package:frontend/themes/style.dart';
+import 'package:frontend/widgets/custom_app_bar.dart';
+import 'package:frontend/widgets/bottom_nav_bar.dart';
 
 void main() async {
   await dotenv.load(fileName: 'assets/config/.env');
@@ -26,20 +31,47 @@ void main() async {
   );
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   final String appName = Constants.appName;
+
+  // 선택된 화면의 인덱스
+  int _selectedIndex = 0;
+
+  // 화면을 전환하는 함수
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  // 각 화면에 해당하는 위젯
+  final List<Widget> _screens = [
+    const HomeScreen(),
+    const NewsScreen(),
+    const LeagueScreen(),
+    const FollowingScreen(),
+    const SettingsScreen(),
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: CustomAppBar(),
+    return Scaffold(
+      appBar: const CustomAppBar(),
       body: Padding(
         padding: bodyPadding,
-        child: HomeScreen(),
+        child: _screens[_selectedIndex],
       ),
-      bottomNavigationBar: BottomNavBar(),
+      bottomNavigationBar: BottomNavBar(
+        selectedIndex: _selectedIndex,
+        onTap: _onItemTapped,
+      ),
     );
   }
 }
