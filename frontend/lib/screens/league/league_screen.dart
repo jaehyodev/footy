43ff3985edfbb7d0ea522dd32ebfb4f6eb4e6811
Jaehyo <kimjaehyo0623@gmail.com>
2024-12-03 17:15:08 +0900

    feat: 리그 선택 기능 수정 및 리그 화면에 적용

--- frontend/lib/screens/league/league_screen.dart
+++ frontend/lib/screens/league/league_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:frontend/widgets/leagues_list.dart';
 
 class LeagueScreen extends StatefulWidget {
   const LeagueScreen({super.key});
@@ -10,6 +11,10 @@ class LeagueScreen extends StatefulWidget {
 class LeagueScreenState extends State<LeagueScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('League Screen'));
+    return const Column(
+      children: <Widget>[
+        LeaguesList(includesAll: false),
+      ],
+    );
   }
 }
