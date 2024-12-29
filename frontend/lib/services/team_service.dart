7931c50868b7341f40794665abe67a630e6406f2
Jaehyo <kimjaehyo0623@gmail.com>
2024-12-29 17:17:50 +0900

    test: player json 테스트

--- frontend/lib/services/team_service.dart
+++ frontend/lib/services/team_service.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:frontend/models/player_in_korean.dart';
 import 'package:http/http.dart' as http;
 
 import 'package:frontend/models/squad_response.dart';
@@ -16,7 +17,7 @@ class TeamService {
       if (response.statusCode == 200) {
         final Map<String, dynamic> data = jsonDecode(response.body);
         // 팀 리스트에서 id가 teamId와 일치하는 팀을 찾음
-        print(data['teams'].firstWhere((team) => team['id'] == teamId));
+        print(data);
         return data['teams'].firstWhere((team) => team['id'] == teamId);
       } else {
         throw Exception('Failed to load team');
@@ -28,25 +29,25 @@ class TeamService {
 
   // 특정 팀의 선수들을 가져오는 메서드 (한국어)
   Future<List<Map<String, dynamic>>> fetchPlayersByKorean(int teamId) async {
-    try {
-      final response = await http.get(Uri.parse(
-          'https://port-0-footy-m415nzyb5a6ffceb.sel4.cloudtype.app/players'));
+    print("받은 팀 아이디:$teamId");
 
-      if (response.statusCode == 200) {
-        final data = jsonDecode(response.body);
-        // players 배열에서 teamId가 맞는 선수만 필터링 후 List로 변환
-        List<dynamic> players = data['players'];
-        // players 리스트에서 teamId와 일치하는 선수만 필터링하고, Map으로 변환
-        return players
-            .where((player) => player['id'].startsWith(teamId))
-            .map<Map<String, dynamic>>(
-                (player) => player as Map<String, dynamic>)
-            .toList(); // Iterable을 List로 변환
-      } else {
-        throw Exception('Failed to load players');
-      }
-    } catch (e) {
-      throw Exception('Error fetching players: $e');
+    final response = await http.get(Uri.parse(
+        'https://port-0-footy-m415nzyb5a6ffceb.sel4.cloudtype.app/players'));
+
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+
+      // players 배열에서 teamId에 맞는 선수만 필터링
+      List<dynamic> players = data['players']
+          .where((player) => player['teamId'] == teamId) // teamId에 맞는 선수만 필터링
+          .toList();
+
+      // 선수들을 Map<String, dynamic> 형태로 리턴
+      return players
+          .map<Map<String, dynamic>>((player) => player as Map<String, dynamic>)
+          .toList();
+    } else {
+      throw Exception('Failed to load players');
     }
   }
 
