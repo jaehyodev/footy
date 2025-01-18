dda75cef3ecc3f743c2c20e0da23786742709552
Jaehyo <kimjaehyo0623@gmail.com>
2025-01-18 19:10:43 +0900

    feat(following): 팔로잉 팀 Firestore 연동 기능 추가

--- frontend/lib/providers/following_provider.dart
+++ frontend/lib/providers/following_provider.dart
@@ -1,5 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 class FollowingProvider extends ChangeNotifier {
   // 상태 관리 필드
@@ -17,15 +18,28 @@ class FollowingProvider extends ChangeNotifier {
   }
 
   // 로컬 저장소에서 팔로잉 팀 목록 로드
+  // Future<void> _loadFollowingTeams() async {
+  //   SharedPreferences prefs = await SharedPreferences.getInstance();
+  //   // 저장된 리스트를 불러와서 Set<int>로 변환
+  //   List<String>? followingTeamsStringList =
+  //       prefs.getStringList('followingTeams');
+  //   followingTeams =
+  //       followingTeamsStringList?.map((e) => int.tryParse(e) ?? 0).toSet() ??
+  //           {};
+  //   notifyListeners();
+  // }
+
+  // firestore에서 팔로잉 팀 목록 로드
   Future<void> _loadFollowingTeams() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    // 저장된 리스트를 불러와서 Set<int>로 변환
-    List<String>? followingTeamsStringList =
-        prefs.getStringList('followingTeams');
-    followingTeams =
-        followingTeamsStringList?.map((e) => int.tryParse(e) ?? 0).toSet() ??
-            {};
-    notifyListeners();
+    User? user = FirebaseAuth.instance.currentUser;
+    if (user != null) {
+      final snapshot = await FirebaseFirestore.instance
+          .collection('user')
+          .doc(user.uid)
+          .get();
+      followingTeams = Set<int>.from(snapshot['followingTeams'] ?? []);
+      notifyListeners();
+    }
   }
 
   // 특정 팀의 팔로우 상태를 토글
@@ -40,11 +54,29 @@ class FollowingProvider extends ChangeNotifier {
   }
 
   // 로컬 저장소에 팔로잉 팀 목록 저장
+  // Future<void> _saveFollowingTeams() async {
+  //   SharedPreferences prefs = await SharedPreferences.getInstance();
+  //   // Set<int>을 List<String>으로 변환하여 저장
+  //   prefs.setStringList(
+  //       'followingTeams', followingTeams.map((e) => e.toString()).toList());
+  // }
+
+  // firestore에 팔로잉 팀 목록 저장
   Future<void> _saveFollowingTeams() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    // Set<int>을 List<String>으로 변환하여 저장
-    prefs.setStringList(
-        'followingTeams', followingTeams.map((e) => e.toString()).toList());
+    try {
+      User? user = FirebaseAuth.instance.currentUser;
+      if (user != null) {
+        await FirebaseFirestore.instance
+            .collection('user')
+            .doc(user.uid)
+            .update({
+          'followingTeams': followingTeams,
+        });
+        print('성공');
+      }
+    } catch (e) {
+      print(e);
+    }
   }
 
   // 팔로잉 팀 목록 반환
